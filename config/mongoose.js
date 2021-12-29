4a784f1972cd1d006bbebd837669e30f106b4b2e
Leafy2001 <51094578+Leafy2001@users.noreply.github.com>
2021-12-29 23:29:25 +0530

    d

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,13 +1,17 @@
 const mongoose = require('mongoose');
 
 // mongoose.connect('mongodb://localhost/codeial_development');
-// mongoose.connect('mongodb+srv://shashank_1234:dellinspiron@cluster0.qmcyz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority');
-mongoose.connect(process.env.MONGODB_URI)
+mongoose.connect('mongodb+srv://shashank_1234:dellinspiron@cluster0.qmcyz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', 
+{
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true
+ });
+// mongoose.connect(process.env.MONGODB_URI)
 
 const db = mongoose.connection;
 
 db.on('error', console.error.bind(console, "Error connecting to MongoDB"));
-
 
 db.once('open', function(){
     console.log('Connected to Database :: MongoDB');
